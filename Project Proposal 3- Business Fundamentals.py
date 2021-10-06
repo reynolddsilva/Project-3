c6c21da7e558b5db63473cbc76516167b8ab910f
reynolddsilva <85266047+reynolddsilva@users.noreply.github.com>
2021-10-06 00:01:25 -0400

    Add files via upload

---
+++ Project Proposal 3- Business Fundamentals.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Project Proposal
+
+# ## Abstract:
+# Climate change is one of the biggest issues faced by the modern world. Transitioning to sustainable energy sources is the need of the hour to avoid future catastrophic events. China is leading the world in this transition and accounted for fourty one percent of all electric vehicles sold worldwide in 2020. According to the Annual US Budget 2021, Biden Administration aims to build 500,000 Electric vehicle charging stations across America by 2030. EV vehicles in the US get a tax credit of $7500 on average. Tesla- market leader in the luxury EV segment, plans to expand to 15000 supercharger stations by 2030 with over 200,000 charging points pan America. It will be safe to assume that other private electric car manufacturers will also expand their network to meet consumer demand. California will ban ICE vehicles by 2030 and plans to build 300,000 charging stations in the next ten years. This sector will most likely see alot of public-private partnerships in the coming decades.
+
+# ## Problem:
+#  To help my client choose appropriate EV charging locations in New York, that gives them an edge over competitors. 
+
+# ## Impact Hypothesis:
+# To help select EV charging station locations across New York, with an intent to maximize utility and build partnerships with third party providers, in case of non-availability of commercial space at strategic locations. 
+# 
+# ## Measures of Success:
+# 1) Technical: Build a model that effectively predicts areas with increased usage, to assist decisions in installing EV charging stations.
+# 
+# 2) Non-Technical: Month-on-month growth in Utility of individual charging stations.
+# 
+
+# ## Data:
+# Energy and Environment data provided by New York City Department
+
+# ## Tools:
+# Excel and Tableau
+
+# In[ ]:
+
+
+
+
